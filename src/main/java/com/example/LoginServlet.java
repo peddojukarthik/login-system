6aec03c86e4ca3f4b1b2d6bd32deab13b11c303c
peddojukarthik <karthikpeddoju1006@gmail.com>
2025-06-20 20:21:37 +0530

    jdbc,4html

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -2,13 +2,18 @@ package com.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
+@WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -16,13 +21,28 @@ public class LoginServlet extends HttpServlet {
         String user = request.getParameter("username");
         String pass = request.getParameter("password");
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        if ("admin".equals(user) && "1234".equals(pass)) {
-            out.println("<h1>Login Success</h1>");
-        } else {
-            out.println("<h1>Login Failed</h1>");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/loguser", "root", "Mextscholar@2007");
+            string query = "select * from users where username=? and password=?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            if( rs.next()){
+                response.getWriter().println("you have successfully logged in");
+            }
+            else{
+                response.getWriter().println("login failed. try again or sign in first.");
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("db error");
         }
     }
 }
