731bb2f4ab35908dfd335836b498dd767968e028
peddojukarthik <karthikpeddoju1006@gmail.com>
2025-06-23 21:00:38 +0530

    error free.success

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -26,19 +26,24 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
         String action = request.getParameter("action");
 
-        System.out.println("servlet called with action:"+action);
-        System.out.println("servlet called with username:"+username);
-        System.out.println("servlet called with password:"+password);
+        
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+             Class.forName("com.mysql.cj.jdbc.Driver");
 
            Connection conn = DriverManager.getConnection( "jdbc:mysql://localhost:3306/loguser","root","Mextscholar@2007"
     );
+    
+    System.out.println("action received:"+action);
             response.setContentType("text/html");
             if ("login".equals(action)) {
+
                 
-            String query = "select * from users where username=? and password=?";
+            String query = "select * from users where username=? and password=?"; 
+
+             response.setContentType("text/html");
+        response.getWriter().println("<html><body><h1>login QUERY</h1></body></html>");
+
             PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setString(1, username);
             pstmt.setString(2, password);
@@ -55,18 +60,20 @@ public class LoginServlet extends HttpServlet {
             rs.close();
             pstmt.close();
         }
-        else if ("signin".equals(action)) {
+        else if ("SIGNIN".equals(action)) {
+
             String iquery ="INSERT INTO users VALUES (?,?)";
             PreparedStatement pstmt = conn.prepareStatement(iquery);
             pstmt.setString(1, username);
             pstmt.setString(2, password);
             int result = pstmt.executeUpdate();
             if (result>0) {
-                 RequestDispatcher rd = request.getRequestDispatcher("signsuccess.html");
-                rd.forward(request, response);
+                response.sendRedirect("signsuccess.html");
+               
             }else{
-                 RequestDispatcher rd = request.getRequestDispatcher("signfail.html");
-                rd.forward(request, response);
+                 response.sendRedirect("signfail.html");
+               
+              
             }
             pstmt.close();
 
@@ -76,7 +83,8 @@ public class LoginServlet extends HttpServlet {
            
         } catch (Exception e) {
             e.printStackTrace();
-            response.getWriter().println("db error" + e.getMessage());
+            response.getWriter().println("<h2>e.exception:"+e.getClass().getName());
+             response.getWriter().println("<p>"+e.getMessage()+"</p>");
         }
     }
 }
